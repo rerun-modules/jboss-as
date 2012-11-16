f9b2bace34713b8c543fc93e0ebf4e00ee35c64b
connaryscott <connaryscott@gmail.com>
2012-11-16 09:06:41 -0800

    implement use of rpm module upgrade to idempotently install the jboss package
    Fixes #4

--- tests/install-1-test.sh
+++ tests/install-1-test.sh
@@ -15,25 +15,5 @@ it_fails_without_any_arguments() {
    rerun jboss-as: install || { true; }
 }
 
-it_runs_with_install_url() {
-
-   buildTmp="$(mktemp -d)"
-
-   cp -r $RERUN_MODULES/jboss-as/examples/build/jboss "${buildTmp}"
-
-   rerun jboss-as:build -v 7.1.1.Final -d "${buildTmp}"/jboss
-
-   # check the package:
-   rpm -qi -p "${buildTmp}"/jboss/RPMS/noarch/jboss-as-7.1.1.Final-1.noarch.rpm
-
-   rpm -qlv -p "${buildTmp}"/jboss/RPMS/noarch/jboss-as-7.1.1.Final-1.noarch.rpm
-
-   rpm -q --requires -p "${buildTmp}"/jboss/RPMS/noarch/jboss-as-7.1.1.Final-1.noarch.rpm
-
-   sudo yum -y --nogpgcheck localinstall "file://${buildTmp}/jboss/RPMS/noarch/jboss-as-7.1.1.Final-1.noarch.rpm"
-
-   rm -rf "${buildTmp}"
-}
-
 # ------------------------------
 
