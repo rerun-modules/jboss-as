044276910b4788c702cfc8b550839773eeb20fa2
connaryscott <connaryscott@gmail.com>
2012-11-27 09:58:10 -0800

    add new trivial configure command Fixes #7

---
+++ tests/configure-1-test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env roundup
+#
+# This file contains the test plan for the configure command.
+# Execute the plan by invoking: 
+#    
+#     rerun stubbs:test -m jboss-as -p configure
+#
+
+# Helpers
+#
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+
+describe "configure"
+
+
+# ------------------------------
+# Replace this test. 
+it_runs_with_no_arguments() {
+    exit 0
+}
+# ------------------------------
+
